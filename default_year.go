f7a1fe6af4afc898464ed849bbcc9f84f2c8f6ad
LiteHell <litehell@litehell.info>
2023-07-24 03:08:02 +0900

    Port yearDefaults into golang

---
+++ default_year.go
@@ -0,0 +1,12 @@
+package main
+
+import "time"
+
+func DefaultYear() (int, int) {
+	now := time.Now()
+	if now.Month() == time.December {
+		return 2004, now.Year() + 1
+	} else {
+		return 2004, now.Year()
+	}
+}
--- yearDefaults.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => {
-    let now = new Date();
-    return {
-        fromDefault: 2004,
-        toDefault: now.getMonth() == 11 ? now.getFullYear() + 1 : now.getFullYear()
-    };
-}
